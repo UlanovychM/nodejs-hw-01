5dbede56bbe4a84859a8803d0d69cecb59f6954b
Mykhailo <m.ulanovych@gmail.com>
2024-05-25 13:46:32 +0300

    'update'

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -3,7 +3,10 @@ import { PATH_DB } from '../constants/contacts.js';
 
 export const thanos = async () => {
   try {
-    await fs.writeFile(PATH_DB, JSON.stringify([]));
+    const buffer = await fs.readFile(PATH_DB);
+    const data = JSON.parse(buffer.toString());
+    const allUsersRandom = data.filter(() => Math.random() >= 0.5);
+    await fs.writeFile(PATH_DB, JSON.stringify(allUsersRandom, null, 2));
   } catch (error) {
     console.log(error);
   }
